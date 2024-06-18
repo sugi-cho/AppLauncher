5881130695809b86ff040c0d4a1de9a4b0ead7f3
sugino <git@sugi.cc>
2024-06-18 18:54:24 +0900

    update script

--- Assets/Scripts/AppControl.cs
+++ Assets/Scripts/AppControl.cs
@@ -1,31 +1,56 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 using SimpleFileBrowser;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+using Debug = UnityEngine.Debug;
+using System;
+using System.Runtime.InteropServices;
 
 public class AppControl : MonoBehaviour
 {
+
+    [DllImport("User32.dll")]
+    private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
     string FilePath => Path.Combine(Application.streamingAssetsPath, $"{typeof(Setting)}.json");
 
     [SerializeField] Setting setting;
+    bool _listening;
+
     // Start is called before the first frame update
     void Start()
     {
+        var json = "";
+        if (!File.Exists(FilePath))
+        {
+            json = JsonUtility.ToJson(setting);
+            File.WriteAllText(FilePath, json);
+        }
+        else
+        {
+            json = File.ReadAllText(FilePath);
+            setting = JsonUtility.FromJson<Setting>(json);
+        }
+
         var doc = GetComponent<UIDocument>();
         var root = doc.rootVisualElement;
         var treeSource = doc.visualTreeAsset;
         var container = root.Q<ScrollView>("Container");
+        var senderView = root.Q("TestMessageSend");
 
         setting.settings.ForEach(setting =>
         {
             var ui = SetupSettingUI(setting);
             container.Add(ui);
+            StartListenerAsync(setting).Cancel();
         });
+        SetupSenderUI(setting.senderSetting);
 
         VisualElement SetupSettingUI(ListenerSetting listenerSetting)
         {
@@ -33,12 +58,10 @@ public class AppControl : MonoBehaviour
 
             var messageField = ui.Q<TextField>("Message");
             var protocolField = ui.Q<EnumField>("ProtocolField");
-            var ipField = ui.Q<DropdownField>("IPField");
-            var portField = ui.Q<TextField>("PortField");
+            var portField = ui.Q<IntegerField>("PortField");
 
             var fileNameField = ui.Q<TextField>("FileField");
             var openButton = ui.Q<Button>("OpenButton");
-            var killToggle = ui.Q<Toggle>("KillToggle");
 
             {
                 messageField.value = listenerSetting.message;
@@ -47,43 +70,158 @@ public class AppControl : MonoBehaviour
                 protocolField.value = listenerSetting.type;
                 protocolField.RegisterValueChangedCallback(evt => listenerSetting.type = (ProtocolType)evt.newValue);
 
-                var ips = Dns.GetHostAddresses(Dns.GetHostName())
-                    .Where(ip=>ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                    .Select(ip => ip.ToString());
-                ipField.choices.AddRange(ips);
-                ipField.value = listenerSetting.ip;
-                ipField.RegisterValueChangedCallback(evt => listenerSetting.ip = evt.newValue);
-                if (ipField.index < 0)
-                {
-                    ipField.index = 0;
-                    listenerSetting.ip = ipField.value;
-                }
-
-                portField.value = listenerSetting.port.ToString();
-                portField.RegisterValueChangedCallback(evt =>
-                {
-                    if (int.TryParse(evt.newValue, out var val))
-                        listenerSetting.port = val;
-                    else
-                        portField.SetValueWithoutNotify(evt.previousValue);
-                });
+                portField.value = listenerSetting.port;
+                portField.RegisterValueChangedCallback(evt => listenerSetting.port = evt.newValue);
             }
             {
                 fileNameField.value = listenerSetting.filePath;
                 fileNameField.RegisterValueChangedCallback(evt => listenerSetting.filePath = evt.newValue);
-                openButton.clicked += () => {
-                    FileBrowser.ShowLoadDialog(fileName => fileNameField.value = fileName[0], () => { }, FileBrowser.PickMode.Files, title:"Select File");
+                openButton.clicked += () =>
+                {
+                    FileBrowser.ShowLoadDialog(fileName => fileNameField.value = fileName[0], () => { }, FileBrowser.PickMode.Files, title: "Select File");
                 };
-                killToggle.value = listenerSetting.useKillMessage;
-                killToggle.RegisterValueChangedCallback(evt => listenerSetting.useKillMessage = evt.newValue);
             }
 
             return ui;
+        }
+
+        void SetupSenderUI(SenderSetting senderSetting)
+        {
+            var messageField = senderView.Q<TextField>("MessageField");
+            var typeField = senderView.Q<EnumField>();
+            var button = senderView.Q<Button>();
+            var ipField = senderView.Q<TextField>("IPField");
+            var portField = senderView.Q<IntegerField>("PortField");
+
+            messageField.value = senderSetting.message;
+            typeField.value = senderSetting.type;
+            ipField.value = senderSetting.ip;
+            portField.value = senderSetting.port;
+
+            messageField.RegisterValueChangedCallback(evt => senderSetting.message = evt.newValue);
+            typeField.RegisterValueChangedCallback(evt => senderSetting.type = (ProtocolType)evt.newValue);
+            ipField.RegisterValueChangedCallback(evt => senderSetting.ip = evt.newValue);
+            portField.RegisterValueChangedCallback(evt => senderSetting.port = evt.newValue);
+
+            ipField.RegisterValueChangedCallback(evt =>
+            {
+                if (!IPAddress.TryParse(evt.newValue, out var val))
+                    ipField.SetValueWithoutNotify(evt.previousValue);
+            });
+
+            button.clicked += () =>
+            {
+                var message = messageField.value;
+                var type = (ProtocolType)typeField.value;
+                var data = System.Text.Encoding.UTF8.GetBytes(message);
+                var port = portField.value;
+                if (IPAddress.TryParse(ipField.value, out var ip))
+                {
+                    var remoteEP = new IPEndPoint(ip, port);
+                    Debug.Log(remoteEP.ToString());
+                    switch (type)
+                    {
+                        case ProtocolType.UDP:
+                            using (var client = new UdpClient())
+                            {
+                                client.Send(data, data.Length, remoteEP);
+                            }
+                            break;
+                        case ProtocolType.TCP:
+                            using (var client = new TcpClient(ipField.value, port))
+                            using (var stream = client.GetStream())
+                            {
+                                stream.Write(data, 0, data.Length);
+                            }
+                            break;
+                    }
+                }
+            };
+        }
+    }
+
+    async Awaitable StartListenerAsync(ListenerSetting listenerSetting)
+    {
+        await Awaitable.BackgroundThreadAsync();
+        Process process = null;
+        _listening = true;
+
+        if (listenerSetting.type == ProtocolType.UDP)
+        {
+            Debug.Log("wait for udp");
+            await ReceiveUDPAsync(listenerSetting.port);
+        }
+        else
+        {
+            Debug.Log("wait for tcp");
+            await ReceiveTCPAsync(listenerSetting.port);
+        }
+
+        StartListenerAsync(listenerSetting).Cancel();
+
+        async Awaitable ReceiveUDPAsync(int port)
+        {
+            using (var client = new UdpClient(port))
+            {
+                while (_listening)
+                {
+                    var result = await client.ReceiveAsync();
+                    var buffer = result.Buffer;
+                    var message = System.Text.Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+                    OnMessageReceive(message).Cancel();
+                }
+            }
+        }
+
+        async Awaitable ReceiveTCPAsync(int port)
+        {
+            var buffer = new byte[4096];
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            while (_listening)
+            {
+                using (var client = await listener.AcceptTcpClientAsync())
+                using (var stream = client.GetStream())
+                {
+                    var size = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    var message = System.Text.Encoding.UTF8.GetString(buffer, 0, size);
+                    OnMessageReceive(message).Cancel();
+                }
+            }
+            listener.Stop();
+        }
+
+        async Awaitable OnMessageReceive(string msg)
+        {
+            await Awaitable.MainThreadAsync();
+            if (msg == listenerSetting.message)
+            {
+                if (process != null && !process.HasExited)
+                    process.Kill();
+                process = Process.Start(listenerSetting.filePath);
+
+                while (process.MainWindowHandle == (IntPtr)0)
+                    await Awaitable.WaitForSecondsAsync(0.1f);
+
+                IntPtr HWND_TOPMOST = new IntPtr(-1);
+                const uint SWP_NOSIZE = 0x0001;
+                const uint SWP_SHOWWINDOW = 0x0040;
+                Debug.Log("hWnd:" + process.MainWindowHandle);
+
+                SetWindowPos(process.MainWindowHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_SHOWWINDOW);
+            }
+            if (msg == listenerSetting.message + "-kill")
+            {
+                if (process != null && !process.HasExited)
+                    process.Kill();
+            }
         }
     }
 
     private void OnDestroy()
     {
+        _listening = false;
         var json = JsonUtility.ToJson(setting);
         File.WriteAllText(FilePath, json);
     }
@@ -93,19 +231,26 @@ public class AppControl : MonoBehaviour
         UDP, TCP
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct Setting
     {
         public List<ListenerSetting> settings;
+        public SenderSetting senderSetting;
     }
-    [System.Serializable]
+    [Serializable]
     public class ListenerSetting
+    {
+        public string message;
+        public ProtocolType type;
+        public int port;
+        public string filePath;
+    }
+    [Serializable]
+    public class SenderSetting
     {
         public string message;
         public ProtocolType type;
         public string ip;
         public int port;
-        public string filePath;
-        public bool useKillMessage;
     }
 }
